2c42f4817217e1a0ff3c581aff408c6f8ad795ff
Dudkin Timofey <dudkinta@gmail.com>
2024-11-26 23:10:16 +0300

    no message

--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -1,20 +1,47 @@
 import crypto from "crypto";
 import { Transaction } from "../blockchain/models/transaction.js";
+import * as fs from "fs";
+import * as path from "path";
 
 export class Wallet {
   private privateKey: string;
   public publicKey: string;
+  private readonly privateKeyPath = path.join(
+    __dirname,
+    "./data/keys/private.key"
+  );
+  private readonly publicKeyPath = path.join(
+    __dirname,
+    "./data/keys/public.key"
+  );
 
   constructor() {
-    // Генерация ключей
-    const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-      modulusLength: 2048,
-      publicKeyEncoding: { type: "spki", format: "pem" },
-      privateKeyEncoding: { type: "pkcs8", format: "pem" },
-    });
+    if (this.keysExist()) {
+      // Восстановление ключей из файлов
+      this.privateKey = fs.readFileSync(this.privateKeyPath, "utf-8");
+      this.publicKey = fs.readFileSync(this.publicKeyPath, "utf-8");
+    } else {
+      // Генерация новых ключей
+      const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+        publicKeyEncoding: { type: "spki", format: "pem" },
+        privateKeyEncoding: { type: "pkcs8", format: "pem" },
+      });
 
-    this.privateKey = privateKey;
-    this.publicKey = publicKey;
+      this.privateKey = privateKey;
+      this.publicKey = publicKey;
+
+      // Сохранение ключей в файлы
+      fs.writeFileSync(this.privateKeyPath, privateKey, "utf-8");
+      fs.writeFileSync(this.publicKeyPath, publicKey, "utf-8");
+    }
+  }
+
+  // Проверка наличия файлов с ключами
+  private keysExist(): boolean {
+    return (
+      fs.existsSync(this.privateKeyPath) && fs.existsSync(this.publicKeyPath)
+    );
   }
 
   // Подписание транзакции
