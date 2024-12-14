8684aa1e0a67c8ac09b3e8142823712782290e7e
DudkinTA <dudkinta@yandex.ru>
2024-12-14 19:30:27 +0200

    no message

--- src/network/services/messages/messages.ts
+++ src/network/services/messages/messages.ts
@@ -136,7 +136,7 @@ export class MessagesService
         console.log(err);
       }
     });
-    //this.components.pubsub.topicValidators.set(MessageType[MessageType.HEAD_BLOCK_INDEX], this.filterMessages.bind(this));
+    this.components.pubsub.topicValidators.set(MessageType[MessageType.HEAD_BLOCK_INDEX], this.filterMessages.bind(this));
     this.components.pubsub.topicValidators.set(MessageType[MessageType.REQUEST_CHAIN], this.filterMessages.bind(this));
   }
 
@@ -148,26 +148,26 @@ export class MessagesService
     const ProtobufMessageChain = this.proto_root.lookupType('MessageChain');
     const bufferMessage = ProtobufMessageChain.decode(msg.data);
     const message = MessageChain.fromProtobuf(bufferMessage);
-    const blockchain = BlockChain.getInstance();
-    if (blockchain) {
-      /*if (message.type == MessageType.HEAD_BLOCK_INDEX) {
-        const msgHeadIndex = message.value as number;
-        if (msgHeadIndex <= blockchain.getHeadIndex()) {
-          this.log(LogLevel.Info, 'Ignore message: HEAD_BLOCK_INDEX');
-          return TopicValidatorResult.Ignore;
+    if (message.sender != this.components.peerId.toString()) {
+      const blockchain = BlockChain.getInstance();
+      if (blockchain) {
+        if (message.type == MessageType.HEAD_BLOCK_INDEX) {
+          const msgHeadIndex = message.value as number;
+          if (msgHeadIndex <= blockchain.getHeadIndex()) {
+            this.log(LogLevel.Info, 'Ignore message: HEAD_BLOCK_INDEX');
+            return TopicValidatorResult.Ignore;
+          }
         }
-
-      }*/
-      if (message.type == MessageType.REQUEST_CHAIN) {
-        const requestChainMessage = message.value as MessageRequest;
-        if (requestChainMessage && requestChainMessage.index < blockchain.getHeadIndex()) {
-          this.log(LogLevel.Info, 'Ignore message: REQUEST_CHAIN');
-          return TopicValidatorResult.Ignore;
+        if (message.type == MessageType.REQUEST_CHAIN) {
+          const requestChainMessage = message.value as MessageRequest;
+          if (requestChainMessage && requestChainMessage.index < blockchain.getHeadIndex()) {
+            this.log(LogLevel.Info, 'Ignore message: REQUEST_CHAIN');
+            return TopicValidatorResult.Ignore;
+          }
         }
       }
     }
     return TopicValidatorResult.Accept;
-
   }
 
   private async messageHandler(evt: CustomEvent<Message>): Promise<void> {
